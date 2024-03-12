f61a6271ab974d3f1a430c171e833cc5f1443eec
Bhumika gupta <bg0674079@gmail.com>
2024-03-12 17:24:05 +0530

    Boillerplate/api

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv").config({path: "./.env"})
+const express = require('express');
+const app = express();
+// const dotenv = require("dotenv");
+// dotenv.config({path: "./.env"});
+
+
+
+// const PORT = process.env.PORT;
+
+app.listen(process.env.PORT,() =>{
+	console.log(`Server is running on port ${process.env.PORT}`);
+})
