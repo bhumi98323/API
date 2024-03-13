7f9ec71fb08f005df8381e18e7e0ca825602e9d8
Bhumika gupta <bg0674079@gmail.com>
2024-03-13 19:46:20 +0530

    boilerplate/api

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require("dotenv").config({path: "./.env"})
 const express = require('express');
 const app = express();
+
+
+require("./models/dbconfig").dbconnection();
+
 // const dotenv = require("dotenv");
 // dotenv.config({path: "./.env"});
-
-
-
 // const PORT = process.env.PORT;
 
 app.listen(process.env.PORT,() =>{
