d104cad42f1fab0782191cb0558946a3004945cf
Bhumika gupta <bg0674079@gmail.com>
2024-03-14 18:42:31 +0530

    db connect

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 require("dotenv").config({path: "./.env"})
 const express = require('express');
 const app = express();
-
-
+//databse
 require("./models/dbconfig").dbconnection();
+//route
+const userRouter = require('./routes/userRoute');
+
+//logger 
+// morgan using for check that koun sa route hit hua h
+const logger = require("morgan");
+app.use(logger("tiny"));
+// app.use(require("morgan")("tiny"))
+
+// body parser  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/user", userRouter)
+
+
+
+
+
+
+
 
 // const dotenv = require("dotenv");
 // dotenv.config({path: "./.env"});
---
+++ routes/userRoute.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+// /api/user + /
+
+router.get("/", function(req,res,next){
+	res.status(200).json({success : true , message: "This is test route"
+	})
+});
+
+router.post("/create", function(req,res,next){
+	res.status(201).json({success: true, user: req.body});
+})
+
+
+module.exports = router;
